8ca091254f0c6e3ff074513065692a119d3e2e6d
Zibzeus <82931864+Zibzeus@users.noreply.github.com>
2022-12-18 22:51:45 +0700

    edit connection and migration
    make new connection and migration from mysql to ms sql, some bug for ex missing waroeng.user cuz in mysql its dont user . before table name

--- config/database.js
+++ config/database.js
@@ -1,36 +1,48 @@
 // database module
-var mysql = require('mysql');
+var mysql = require('mssql');
+
+// config database
 var config = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'waroeng'
+    user: 'sa',
+    password: 'yourStrong(!)Password',
+    server: 'localhost',
+    database: 'waroeng',
+    port: 1433,
+    options: {
+        encrypt: true, // Use this if you're on Windows Azure
+        useUTC: true,
+        trustServerCertificate: true
+    }
 };
 
 // init database
-var pool = mysql.createPool(config);
+var pool = new mysql.ConnectionPool(config, function (err) {
+    if (err) {
+        ShowErrors(err);
+    }
+});
 
 //Fetch data
-function RunQuery(sql, callback) {
-    pool.getConnection(function (err, conn) {
+function RunQuery(sqlStr, callback) {
+    var request = new mysql.Request(pool, function (err) {
         if (err) {
             ShowErrors(err);
         }
-        conn.query(sql, function (err, rows, fields) {
-            if (err) {
-                ShowErrors(err);
-            }
-            conn.release();
-            callback(rows);
-        });
+    });
+    request.query(sqlStr, function (err, recordset) {
+        if (err) {
+            ShowErrors(err);
+        }
+        callback(recordset);
     });
 }
 
 //Throw errors
 function ShowErrors(err) {
-    throw err;
+    console.log(err);
 }
 
+// expose this function to our app using module.exports
 module.exports = {
     RunQuery: RunQuery
 };
